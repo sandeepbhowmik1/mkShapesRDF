76621e129961d58d3d457952f80174354b5ff56d
Sergio Blanco Fernandez <60780075+BlancoFS@users.noreply.github.com>
2023-08-03 10:51:16 +0200

    Add formulasToAdd without SF

---
+++ mkShapesRDF/processor/modules/formulasToAdd_MCnoSF_Full2022EEv11.py
@@ -0,0 +1,94 @@
+from mkShapesRDF.processor.framework.module import Module
+
+from mkShapesRDF.processor.data.LeptonSel_cfg import LepFilter_dict, ElectronWP, MuonWP
+
+
+class formulasToAdd_MCnoSF_Full2022EEv11(Module):
+    def __init__(self):
+        super().__init__("formulasToAdd_MCnoSF_Full2022EEv11")
+
+    def runModule(self, df, values):
+        df = df.Define(
+            "METFilter_Common",
+            "Flag_goodVertices * \
+            Flag_globalSuperTightHalo2016Filter * \
+            Flag_EcalDeadCellTriggerPrimitiveFilter * \
+            Flag_BadPFMuonFilter * \
+            Flag_BadPFMuonDzFilter * \
+            Flag_hfNoisyHitsFilter * \
+            Flag_ecalBadCalibFilter",
+        )
+
+        df = df.Define("METFilter_DATA", "METFilter_Common * Flag_eeBadScFilter")
+
+        useGenW = "genWeight" in df.GetColumnNames()
+
+        if useGenW:
+            df = df.Define("XSWeight", "baseW * genWeight")
+        else:
+            df = df.Define("XSWeight", "baseW")
+
+        df = df.Define("_2lepOk", "Lepton_pt.size() > 1")
+        df = df.Define("_3lepOk", "Lepton_pt.size() > 2")
+        df = df.Define("_4lepOk", "Lepton_pt.size() > 3")
+
+
+        muWPlist = [wp for wp in MuonWP["Full2022EEv11"]["TightObjWP"]]
+        eleWPlist = [wp for wp in ElectronWP["Full2022EEv11"]["TightObjWP"]]
+
+        for eleWP in eleWPlist:
+            for muWP in muWPlist:
+
+                df = df.Define(
+                    "LepCut2l__ele_" + eleWP + "__mu_" + muWP,
+                    "_2lepOk ? (Lepton_isTightElectron_"
+                    + eleWP
+                    + "[0]>0.5 || Lepton_isTightMuon_"
+                    + muWP
+                    + "[0]>0.5) && (Lepton_isTightElectron_"
+                    + eleWP
+                    + "[1]>0.5 || Lepton_isTightMuon_"
+                    + muWP
+                    + "[1]>0.5) : false",
+                )
+
+                df = df.Define(
+                    "LepCut3l__ele_" + eleWP + "__mu_" + muWP,
+                    "_3lepOk ? (Lepton_isTightElectron_"
+                    + eleWP
+                    + "[0]>0.5 || Lepton_isTightMuon_"
+                    + muWP
+                    + "[0]>0.5) && (Lepton_isTightElectron_"
+                    + eleWP
+                    + "[1]>0.5 || Lepton_isTightMuon_"
+                    + muWP
+                    + "[1]>0.5) && (Lepton_isTightElectron_"
+                    + eleWP
+                    + "[2]>0.5 || Lepton_isTightMuon_"
+                    + muWP
+                    + "[2]>0.5) : false",
+                )
+
+                df = df.Define(
+                    "LepCut4l__ele_" + eleWP + "__mu_" + muWP,
+                    "_4lepOk ? (Lepton_isTightElectron_"
+                    + eleWP
+                    + "[0]>0.5 || Lepton_isTightMuon_"
+                    + muWP
+                    + "[0]>0.5) && (Lepton_isTightElectron_"
+                    + eleWP
+                    + "[1]>0.5 || Lepton_isTightMuon_"
+                    + muWP
+                    + "[1]>0.5) && (Lepton_isTightElectron_"
+                    + eleWP
+                    + "[2]>0.5 || Lepton_isTightMuon_"
+                    + muWP
+                    + "[2]>0.5) && (Lepton_isTightElectron_"
+                    + eleWP
+                    + "[3]>0.5 || Lepton_isTightMuon_"
+                    + muWP
+                    + "[3]>0.5) : false",
+                )
+
+
+        return df
