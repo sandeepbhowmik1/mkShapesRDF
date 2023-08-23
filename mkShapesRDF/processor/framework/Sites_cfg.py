887ecfea6232ead9abb68c52ff2d69532680478e
Sergio Blanco Fernandez <60780075+BlancoFS@users.noreply.github.com>
2023-08-23 09:28:10 +0200

    Create Sites_cfg.py

---
+++ mkShapesRDF/processor/framework/Sites_cfg.py
@@ -0,0 +1,6 @@
+
+Sites = {
+    "eosTmpWorkDir": "/eos/user/u/username/MC/processor/",
+    "eosDir"       : "/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano",
+    "rediretor"    : "root://cms-xrd-global.cern.ch/",
+}
