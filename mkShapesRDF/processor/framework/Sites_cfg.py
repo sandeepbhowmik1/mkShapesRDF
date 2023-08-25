4b2ede782605c5a27bc12db473431181ed1a8a69
Sergio Blanco Fernandez <60780075+BlancoFS@users.noreply.github.com>
2023-08-25 10:51:14 +0200

    Fix typo

--- mkShapesRDF/processor/framework/Sites_cfg.py
+++ mkShapesRDF/processor/framework/Sites_cfg.py
@@ -1,6 +1,6 @@
 
 Sites = {
     "eosTmpWorkDir": "", ### Optional to redirect the input samples
-    "eosDir"       : "/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano",
+    "eosDir"       : "/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/",
     "rediretor"    : "root://cms-xrd-global.cern.ch/",
 }
