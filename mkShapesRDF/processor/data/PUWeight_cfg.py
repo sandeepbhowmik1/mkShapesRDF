fad36b0cdc2368db34ad78aef0de6732a30afe7e
Sergio Blanco Fernandez <60780075+BlancoFS@users.noreply.github.com>
2023-09-19 12:09:54 +0200

    Update configuration for 2022

--- mkShapesRDF/processor/data/PUWeight_cfg.py
+++ mkShapesRDF/processor/data/PUWeight_cfg.py
@@ -4,12 +4,12 @@ PUCfg = {
     'Full2022EEv11': {
         'srcfile'     : "auto" ,
         'targetfiles' : { '1-1' : '/processor/data/PUweights/2022/2022_PU.root' } ,
-        'srchist'     : "pileup"   ,
-        'targethist'  : "pileup"   ,
+        'srchist'     : ["PV_npvsGood", "Rho_fixedGridRhoFastjetCentralCalo", "Rho_fixedGridRhoFastjetCentralChargedPileUp"],
+        'targethist'  : ["PV_npvsGood", "Rho_fixedGridRhoFastjetCentralCalo", "Rho_fixedGridRhoFastjetCentralChargedPileUp"],
         'name'        : "puWeight" ,
         'norm'        : True       ,
         'verbose'     : False      ,
-        'nvtx_var'    : "Pileup_nTrueInt" ,
+        'nvtx_var'    : ["PV_npvsGood", "Rho_fixedGridRhoFastjetCentralCalo", "Rho_fixedGridRhoFastjetCentralChargedPileUp"],
         'doSysVar'    : True ,
     } ,
 }
