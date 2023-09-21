80a1ada2b35b9ecf2056561a41a038bde6660bdb
Sergio Blanco Fernandez <60780075+BlancoFS@users.noreply.github.com>
2023-09-21 08:50:33 +0200

    Fix typo in MuonEG datasets

--- mkShapesRDF/processor/framework/samples/Run2022_Prompt_nAODv11.py
+++ mkShapesRDF/processor/framework/samples/Run2022_Prompt_nAODv11.py
@@ -14,9 +14,9 @@ Samples["EGamma_Run2022F-Prompt-v1"] = {
     "nanoAOD": "/EGamma/Run2022F-PromptNanoAODv11_v1-v2/NANOAOD"
 }
 
-Samples["MuonEG_Run2022G-Prompt-v1"] = {
+Samples["MuonEG_Run2022F-Prompt-v1"] = {
     "nanoAOD": "/MuonEG/Run2022F-PromptNanoAODv11_v1-v2/NANOAOD"
 }
-Samples["MuonEG_Run2022F-Prompt-v1"] = {
+Samples["MuonEG_Run2022G-Prompt-v1"] = {
     "nanoAOD": "/MuonEG/Run2022G-PromptNanoAODv11_v1-v2/NANOAOD"
 }
